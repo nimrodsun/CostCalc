b9938feb08d5a09903a093589e02e93ccf3fcc05
Alex Abrahams <nimrodsun@gmail.com>
2015-02-15 19:58:23 +0000

    Clean up debug code
    removed a lot of debug code I had commented out.

--- firewallrules.js
+++ firewallrules.js
@@ -27,37 +27,20 @@ function addHG() {
     // Take the New Host Group Form and turn it into an object
     HGAll.push(new HostGroup($("#HGName").val(),$("input:radio[name=HGZone]").val(), $("#HGDesc").val() ));
 
-    //var HGObj = { "HGName":$("#HGName").val(), "HGZone":$("input:radio[name=HGZone]").val(), "HGDesc":$("#HGDesc").val(), "HGAddresses":[]};
-    // HGAll.push(HGObj);
-
-    //Output to console and screen for debugging
-    // console.log(HGAll);
-    //var output = JSON.stringify(HGAll) + JSON.stringify(NGAll);
-    //$("#update").html(output);
     renderHG();
 }
 
 function addNG() {
-
+    // Take the New Network Group Form and turn it into an object
     NGAll.push(new NetworkGroup($("#NGName").val(), $("input:radio[name=NGZone]").val(), $("#NGDesc").val()));
-    //var NGObj = {"NGName":$("#NGName").val(), "NGZone":$("input:radio[name=NGZone]").val(), "NGDesc":$("#NGDesc").val(), "NGNetworks":{}};
-    //NGAll.push(NGObj);
 
-
-    //Output to console and screen for debugging
-    console.log(NGAll);
-    var output = JSON.stringify(HGAll) + JSON.stringify(NGAll);
-    $("#update").html(output);
 }
 
 function renderHG() {
     var output = "<ul>";
-    $.each(HGAll, function(O, HG){
+    $.map(HGAll, function(HG){
         output += "<li> " + HG.name + "</li>";
         console.log(HG);
-        /**$.each(HG, function(Object, Value){
-            output += "<li> Object: " + Object + " And Value: "+ Value + "</li>";
-        }); **/
 
     });
     output += "</ul>";
